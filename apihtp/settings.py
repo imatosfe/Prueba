0f52381464a41675a407d488f20210ccb5f05248
yamelmatos06@gmail.com <yamelmatos06@gmail.com>
2024-11-05 16:37:57 -0400

    Primer commit del proyecto

--- apihtp/settings.py
+++ apihtp/settings.py
@@ -134,12 +134,13 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
