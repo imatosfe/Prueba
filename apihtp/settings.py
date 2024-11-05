b1f565577886f159c3953f91f0eb4b9c996b218c
yamelmatos06@gmail.com <yamelmatos06@gmail.com>
2024-11-05 16:51:59 -0400

    Primer commit del proyecto

--- apihtp/settings.py
+++ apihtp/settings.py
@@ -140,7 +140,14 @@ import os
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Asegúrate de que LOGIN_URL está configurado
+LOGIN_URL = '/accounts/login/'
