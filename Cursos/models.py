07d78c6cdc34a67ae292d3eb9d123591a8b560bf
yamelmatos06@gmail.com <yamelmatos06@gmail.com>
2024-11-07 10:41:11 -0400

    el segundo dolor

--- Cursos/models.py
+++ Cursos/models.py
@@ -7,7 +7,7 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
     profesores = models.ForeignKey(Profesor, on_delete=models.SET_NULL, null=True)
-    fecha_creacion = models.DateField(auto_now_add=True)
+    fecha_creacion = models.DateField()
 
     def __str__(self):
         return self.nombre
