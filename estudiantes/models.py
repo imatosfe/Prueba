371b61a1408bf4b04f4e9b478c1cae39ae5c3245
imatosfe <yamelmatos06@egmail.com>
2024-11-07 01:24:00 -0400

    con dolor de cabeza 7 de noviembre

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -71,7 +71,7 @@ class Estudiante(models.Model):
 
     # Información académica
     matricula = models.CharField(max_length=20, unique=True)
-    fecha_ingreso = models.DateField(default=timezone.now)
+    fecha_ingreso = models.DateField()
    
     grado = models.CharField(max_length=20)  # Ej: 1° de Primaria, 2° de Secundaria
   
