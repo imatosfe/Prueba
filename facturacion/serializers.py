900ec10f3e6c22a579e086385c2f28b4121992e6
imatosfe <yamelmatos06@egmail.com>
2024-11-13 00:32:14 -0400

    insertando vista get de pago

--- facturacion/serializers.py
+++ facturacion/serializers.py
@@ -8,11 +8,20 @@ class TarifaSerializer(serializers.ModelSerializer):
         model = Tarifa
         fields = '__all__'
 
+
 class CuentaPorCobrarSerializer(serializers.ModelSerializer):
+    estudiante_nombre = serializers.CharField(source='estudiante.nombre')
+    seccion_nombre = serializers.CharField(source='seccion.nombre')
+    facturas = serializers.SerializerMethodField()
+
     class Meta:
         model = CuentaPorCobrar
-        fields = '__all__'
+        fields = ['id', 'estudiante_nombre', 'seccion_nombre', 'monto', 'fecha_factura', 'estado', 'facturas']
 
+    def get_facturas(self, obj):
+        # Retornar una lista de facturas asociadas a la cuenta
+        facturas = obj.facturas.all()  # Obtenemos todas las facturas asociadas
+        return [{"id": factura.id, "numero_factura": factura.numero_factura, "total": factura.total} for factura in facturas]
 # facturacion/serializers.py
 from .models import Factura
 
