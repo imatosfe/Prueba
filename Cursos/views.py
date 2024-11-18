d15f5dd7850f95a222b01b27ba8f4095e307c3b9
imatosfe <yamelmatos06@egmail.com>
2024-11-17 20:29:19 -0400

    insertando vista de usuarios

--- Cursos/views.py
+++ Cursos/views.py
@@ -2,7 +2,11 @@
 from rest_framework import viewsets
 from .models import Curso
 from .serializers import CursoSerializer
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 class CursoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]  # El usuario debe estar autenticado
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
